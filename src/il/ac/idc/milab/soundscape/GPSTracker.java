49edc4749b58539de7329a0750db8d771e75eb48
Tal Kammer <talkammer@gmail.com>
2013-03-09 13:12:42 +0200

    Added a skeleton class for GPS

---
+++ src/il/ac/idc/milab/soundscape/GPSTracker.java
@@ -0,0 +1,32 @@
+package il.ac.idc.milab.soundscape;
+
+import android.content.Context;
+ 
+public class GPSTracker {
+
+	public GPSTracker(Context context) 
+	{
+		
+	}
+	
+	public double getLatitude() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double getLongitude() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public boolean canGetLocation() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void showSettingsAlert() {
+		// TODO Auto-generated method stub
+		
+	}
+ 
+}
