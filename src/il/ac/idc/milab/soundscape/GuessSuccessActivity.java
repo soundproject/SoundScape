00672792da58ae05b61e66a8dc5c286b34224262
Gadi Ickowicz <gadi.ick@gmail.com>
2013-05-03 13:08:57 +0300

    GameOver and GuessCorrect add
    Added screens after guessing for both gameover and guess correct
    situations

---
+++ src/il/ac/idc/milab/soundscape/GuessSuccessActivity.java
@@ -0,0 +1,75 @@
+package il.ac.idc.milab.soundscape;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class GuessSuccessActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_guess_success);
+	}
+	
+	private Button m_OKButton;
+	private TextView m_NumberOfStarsTextView;
+	private TextView m_NumberOfBombsTextView;
+	private ProgressBar m_LevelBarProgressBar;
+	
+	@Override
+	protected void onResume() {
+		
+		super.onResume();
+		
+		
+		
+		initButtons();
+		
+	}
+
+	private void initButtons() {
+		
+		this.m_OKButton = (Button) findViewById(R.id.guess_success_back_to_menu_button);
+		this.m_NumberOfBombsTextView = (TextView) findViewById(R.id.guess_success_bomb_textView);
+		this.m_NumberOfStarsTextView = (TextView) findViewById(R.id.guess_success_stars_textView);
+		this.m_LevelBarProgressBar = (ProgressBar) findViewById(R.id.guess_success_progressBar);
+		
+		Bundle extras = getIntent().getExtras();		
+		int difficulty = extras.getInt("difficulty");
+		
+		this.m_NumberOfBombsTextView.setText(String.format("%d", 2));		
+		this.m_NumberOfStarsTextView.setText(String.format("%d", difficulty));		
+		this.m_LevelBarProgressBar.setMax(100);		
+		this.m_LevelBarProgressBar.setProgress(25);
+		
+		this.m_OKButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				startGameLobbyActivity();
+				finish();
+				
+			}
+		});
+		
+	}
+
+	protected void startGameLobbyActivity() {
+		// TODO TAL - how do I start the gameLobby from here?
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_guess_success, menu);
+		return true;
+	}
+
+}
